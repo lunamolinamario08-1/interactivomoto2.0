8f1cd5a2a92f08b936c3bfeb606b343dda6e6ce6
molina08-02 <luna.molina.mario.46@cbtis118.edu.mx>
2025-12-03 18:35:15 +0000

    agrege conexion

---
+++ conexion.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>Concatenar variables en PHP</title>
+    </head>
+    <body>
+        <?php
+            $conexion= mysqli_connect("sql302.infinityfree.com","if0_40475211","WEimgDsvW1Wq","if0_40475211_interactivo");
+            if(!$conexion){
+                echo "Error:no se pudo conectar a MySQL";
+                echo "errno de depuración: " . mysqli_connect_errno();
+                echo "error de depuración: " . mysqli_connect_error();
+                exit;
+            }
+            echo "Conexión exitosa con MySQL";
+            echo "<br>";
+            echo "Información del host: " . mysqli_get_host_info($conexion);
+            mysqli_close($conexion);
+        ?>
+    </body>
+</html>
